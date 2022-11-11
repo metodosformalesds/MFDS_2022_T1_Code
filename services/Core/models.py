50e4cfa4014413a05881d6abca2dfc41632d8f44
192845-AlexisO <al192845@alumnos.uacj.mx>
2022-11-10 20:10:58 -0700

    Update
    Modelo usuario
    Falta Proveedor
    Realizar documentacion

--- services/Core/models.py
+++ services/Core/models.py
@@ -3,4 +3,31 @@ from django.contrib.auth.models import BaseUserManager,AbstractBaseUser, Permiss
 
 # Create your models here.
 
+class UserManager(BaseUserManager):
 
+    def create_user(self, username, email=None, password=None, **extrafields):
+        if not username:
+            raise ValueError('Se debe tener un nombre de usuario')
+        user = self.model(username=username, email=email, **extrafields)
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+
+   
+
+class User(AbstractBaseUser, PermissionsMixin):
+    nombre = models.CharField(max_length=30)
+    apellidoP = models.CharField(max_length=20)
+    apellidoM = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    username = models.CharField(max_length=20, unique=True)
+    fechaDeNacimiento = models.DateField
+    password = models.CharField(max_length=20)
+    telefono = models.IntegerField()
+    tipoCuenta = models.BooleanField(default=False)
+    direccion = models.CharField(max_length=50)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
--- services/services/settings.py
+++ services/services/settings.py
@@ -39,8 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
-
-    'pages',
     'Core',
 ]
 
@@ -131,3 +129,5 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'Core.User'
