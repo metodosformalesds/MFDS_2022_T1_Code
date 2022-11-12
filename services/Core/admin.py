355600b118f7f8bcd9ce82e7515135c33a639f55
192845-AlexisO <al192845@alumnos.uacj.mx>
2022-11-12 16:42:23 -0700

    Update Models
    Modelos terminados
    falta documentar

--- services/Core/admin.py
+++ services/Core/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUser
+from django.utils.translation import gettext as _
+
+from Core import models
 
 # Register your models here.
+
+class UserAdmin(BaseUser):
+    ordering = ['id']
+    list_display = ['id','username', 'email', 'nombre']
+    list_display_links = ['id', 'username']
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        (_('Info personal'),{'fields': ('nombre','apellidoP','apellidoM','telefono')}),
+        (_('Permisos'), {'fields': ('is_active','is_staff','is_superuser')})
+    )
+    add_fieldsets = (
+        (None,{
+            'classes': ('wide',),
+            'fields': ('username','email','telefono', 'password1', 'password2')
+        })
+    )
+
+admin.site.register(models.User, UserAdmin)
--- services/Core/models.py
+++ services/Core/models.py
@@ -8,10 +8,18 @@ class UserManager(BaseUserManager):
     def create_user(self, username, email=None, password=None, **extrafields):
         if not username:
             raise ValueError('Se debe tener un nombre de usuario')
-        user = self.model(username=username, email=email, **extrafields)
+        user = self.model(username=username, email=self.normalize_email(email), **extrafields)
         user.set_password(password)
         user.save(using=self._db)
         
+        return user
+
+    def create_superuser(self, username, password, email=None):
+        user = self.create_user(username, email, password)
+        user.is_staff =True
+        user.is_superuser = True
+        user.save(using=self._db)
+ 
         return user
 
    
@@ -23,10 +31,13 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.CharField(max_length=50)
     username = models.CharField(max_length=20, unique=True)
     fechaDeNacimiento = models.DateField
-    password = models.CharField(max_length=20)
-    telefono = models.IntegerField()
+    password = models.CharField(max_length=100)
+    telefono = models.BigIntegerField(default=0, unique=True)
     tipoCuenta = models.BooleanField(default=False)
     direccion = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
 
     objects = UserManager()
 
