43b6d49839cbeefe510ae9c8a8bd80c0ff2561b2
Jesus Enrique Martinez Perez <111886121+al153974@users.noreply.github.com>
2022-11-16 22:03:46 -0600

    Update import requests.py
    Se sigue actualizando para mejorar el scrap de datos

--- Scraping/import requests.py
+++ Scraping/import requests.py
@@ -1,14 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
 
 path = 'C:/Users/chay9/OneDrive/Desktop/chromedriver.exe'
 options = webdriver.ChromeOptions()
 options.headless = True
 
 driver = webdriver.Chrome(path)
-driver.get('https://ciudadjuarez.locanto.com.mx/Servicios/S/')
+driver.get('https://ciudadjuarez.locanto.com.mx/Autos/902/')
 
-servicios = driver.find_elements(By.XPATH,"//h3[@class='bp_ad__title']")
-print(servicios)
+entradas = driver.find_elements(By.XPATH, "//div[@class='entries']")
+
+for ent in entradas:
+    servicios = driver.find_element(By.XPATH,"//h3[@class='bp_ad__title']")
+    print('\n\n')
+    print(servicios.text)
+
 
 #Se selecciono otra pagina por complicaciones, esta ya esta tomando los diferentes titulos de los servicios
