ee86f2b34def3085769b03a5c884384fa584f58b
Jesus Enrique Martinez Perez <111886121+al153974@users.noreply.github.com>
2022-11-17 12:11:53 -0600

    Update import requests.py
    Extraccion de datos y el csv

--- Scraping/import requests.py
+++ Scraping/import requests.py
@@ -10,10 +10,23 @@ options.headless = True
 driver = webdriver.Chrome(path)
 driver.get('https://ciudadjuarez.locanto.com.mx/Autos/902/')
 
-servicios = driver.find_elements(By.XPATH,"//h3[@class='bp_ad__title']")
-servicios = [ serv.text for serv in servicios ]
-print(servicios)
+servicios_title = driver.find_elements(By.XPATH,"//h3[@class='bp_ad__title']")
+servicios_title = [ serv.text for serv in servicios_title ]
 
+servicios_price = driver.find_elements(By.XPATH, "//div[@class='bp_ad__price']")
+servicios_price = [ pri.text for pri in servicios_price ]
+
+servicios_info = driver.find_elements(By.XPATH, "//a[@class='bp_ad__desc_link']")
+servicios_info = [ inf.text for inf in servicios_info ]
+
+Publicaciones = {
+    "Titulo": servicios_title,
+    "Precio": servicios_price,
+    "Informacion": servicios_info
+}
+
+df = pd.DataFrame(Publicaciones) 
+df.to_csv("Publicaciones.csv")
 #price //div[@class='bp_ad__price']
-#Information //a[@class='bp_ad__desc_link']
+#Information//a[@class='bp_ad__desc_link']
 
