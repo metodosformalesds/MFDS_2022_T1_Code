8646383fc219d1a3edad5a728b6e6156516b826d
Antonio Hernandez <62306021+Antonio180233@users.noreply.github.com>
2022-11-17 22:52:10 -0600

    Documentación de funciones

---
+++ Documentacion de funciones/Listings_docstrings.py
@@ -0,0 +1,19 @@
+def listings(request):
+    """Esta funcion es utilizada para mostrar los anuncios en la pagina de publicaciones destacadas (listings.html)
+    , estos anuncios contienen informacion como categoria, proveedor, titulo, celular y correo electronico. 
+    """
+def listing(request, pk):
+    """Esta funcion es utilizada para mostrar los anuncios en la pagina de publicaciones destacadas (listing.html)
+    , estos anuncios contienen informacion como el titulo, la descripcion, el precio, la categoria, la fecha de publicacion, nombre 
+     del vendedor, la direccion, el numero de telefono, el correo electronico y la imagen o imagenes del anuncio. \
+    """
+def search(request):
+    """Esta funcion es utilizada para buscar los anuncios en la pagina de publicaciones destacadas (search.html)
+    , estas busquedas son realizadas por medio de palabras claves(descriptions), categoria(category_choices) 
+    y precio(price_choices) o ratings(valoraciones por usuarios).
+    """
+
+
+    
+
+
