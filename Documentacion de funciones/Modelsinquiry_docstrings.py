d6fd882e478ac8d3b0873efabdaf3afa6105129e
Jared2920 <60677032+Jared2920@users.noreply.github.com>
2022-11-23 19:47:35 -0700

    Create Modelsinquiry_docstrings.py

---
+++ Documentacion de funciones/Modelsinquiry_docstrings.py
@@ -0,0 +1,17 @@
+class inquiry(models.Model):
+     
+      """
+    Esta clase contiene los modelos de la tabla inquirys de la base de datos
+        , contiene los campos: 
+        
+    listing: Nombre del servicio
+    listing_id: Id del servicio publicado
+    name: Nombre del usuario que solicito el servicio
+    email:  correo del usuario que solicito el servicio
+    phone:  telefono del usuario que solicito el servicio
+    message: mensaje del usuario que solicito el servicio
+    contact_date_ Es la fecha de cuando se realizo la solicitud del servicio
+    user_id ID: del usuario
+    owner_id: ID del dueño de la publicacion 
+    
+    """
